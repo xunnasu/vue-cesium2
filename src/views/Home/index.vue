be0bab3a00a667d2007f0fe971579b6c96ec8d7d
露娜-苏勋娜 <suxunna@holomatic.com>
2024-10-24 14:53:59 +0800

    init3

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -2,6 +2,8 @@
   <div class="v-page home-page flex-grid flex-col">
     首页
     <el-button type="primary" @click="handleClick">弹窗</el-button>
+    <div>1111</div>
+    <div>22222</div>
   </div>
 </template>
 
@@ -11,7 +13,7 @@ import { ElMessageBox } from 'element-plus';
 
 function handleClick() {
   ElMessageBox.confirm('Are you sure to close this dialog?')
-    .then(() => {})
+    .then(() => { })
     .catch(() => {
       // catch error
     });
