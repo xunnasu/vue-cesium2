2b4b668c2078414f8a70a98c09362b2df6b25f16
露娜-苏勋娜 <suxunna@holomatic.com>
2024-10-24 10:23:17 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import '@/assets/styles/main.scss';
+// element fun （自动引入方式需手动引入使用fun调用的css）
+import 'element-plus/es/components/message/style/css';
+import 'element-plus/es/components/message-box/style/css';
+import 'element-plus/es/components/notification/style/css';
+
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
---
+++ src/views/Home/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="v-page home-page flex-grid flex-col">
+    首页
+    <el-button type="primary" @click="handleClick">弹窗</el-button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { ElMessageBox } from 'element-plus';
+
+function handleClick() {
+  ElMessageBox.confirm('Are you sure to close this dialog?')
+    .then(() => {})
+    .catch(() => {
+      // catch error
+    });
+}
+</script>
+
+<style lang="scss" scoped></style>
